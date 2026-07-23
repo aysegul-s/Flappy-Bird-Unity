036450337c475ec27b22e7e0de65a5311403fbe3
Ayşegül Süleymanlıoğlu <aysegulsuleymanliogl@stu.aydin.edu.tr>
2026-07-23 12:45:29 +0300

    Add files via upload

---
+++ Assets/Scpipts/BirdScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class BirdScript : MonoBehaviour
+{
+    public float zıplama_aralığı;
+    Rigidbody2D rb;
+    public Text skor_text;
+    public float skor;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        skor = 0;
+    }
+
+    void Update()
+    {
+        if (Pointer.current != null && Pointer.current.press.wasPressedThisFrame)  // önceden buydu -> if (Input.GetMouseButtonDown(0))
+        {
+            rb.linearVelocity = Vector2.up * zıplama_aralığı;
+        }
+
+        skor_text.text = skor.ToString();
+    }
+
+    void OnTriggerEnter2D(Collider2D temas)
+    {
+      if(temas.gameObject.tag == "Scorer")
+        {
+            skor++;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D temas)
+    {
+        if(temas.gameObject.tag == "pipe")
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+}
